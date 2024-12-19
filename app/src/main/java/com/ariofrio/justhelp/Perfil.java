73281833286de9b943b0a0219236f41f38f1a26e
am4DAM2-Diego <dhercan391@gmail.com>
2024-12-19 10:32:20 +0100

    a local

--- app/src/main/java/com/ariofrio/justhelp/Perfil.java
+++ app/src/main/java/com/ariofrio/justhelp/Perfil.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.dhernandez.justhelp.R;
+import com.ariofrio.justhelp.R;
 
 public class Perfil extends AppCompatActivity {
 
