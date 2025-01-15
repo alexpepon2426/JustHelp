d0932f77b67511e5918d052adc8c0060eaa583c3
alexpepon2426 <alexpepon2426@gmail.com>
2025-01-15 13:28:48 +0100

    barra provisional

--- app/src/main/java/com/ariofrio/justhelp/AniadirO.java
+++ app/src/main/java/com/ariofrio/justhelp/AniadirO.java
@@ -1,13 +1,23 @@
 package com.ariofrio.justhelp;
 
 import android.os.Bundle;
-
+import android.service.controls.actions.FloatAction;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class AniadirO extends AppCompatActivity {
 
     @Override
@@ -15,10 +25,32 @@ public class AniadirO extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_aniadir_o);
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
+        FloatingActionButton myfab = findViewById(R.id.fab);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        myfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AniadirO.this, "Vas a crear un Anuncio", Toast.LENGTH_SHORT).show();
+            }
+        });
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId()==R.id.person)
+                    Toast.makeText(AniadirO.this, "Has pulsado usuario", Toast.LENGTH_SHORT).show();
+                if( item.getItemId()==R.id.location)
+                    Toast.makeText(AniadirO.this, "Has pulsado localización", Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        });
+
+
     }
-}
+    }
+
