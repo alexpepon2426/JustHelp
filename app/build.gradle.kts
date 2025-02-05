164335f026f48eeb82f4ee216cefb052ff3c3a4b
Laura Marcos <lauramarcosmo@gmail.com>
2025-02-05 13:42:33 +0100

    no ejecuta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,8 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+
 }
---
+++ app/src/main/java/com/ariofrio/justhelp/CloudinaryResponse.java
@@ -0,0 +1,16 @@
+package com.ariofrio.justhelp;
+
+public class CloudinaryResponse {
+
+   private String url;
+        private String secure_url;
+
+        public String getUrl() {
+            return url;
+        }
+
+        public String getSecureUrl() {
+            return secure_url;
+        }
+}
+
