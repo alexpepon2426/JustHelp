65ff6b5330c333799860b025f7597b158933ff61
Laura Marcos <lauramarcosmo@gmail.com>
2025-02-19 13:23:42 +0100

    fotos en main y en perfil de la base de datos

--- app/src/main/java/com/ariofrio/justhelp/MyAdapter.java
+++ app/src/main/java/com/ariofrio/justhelp/MyAdapter.java
@@ -5,11 +5,14 @@ import androidx.appcompat.graphics.drawable.DrawableContainerCompat.*;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -18,11 +21,12 @@ import java.util.List;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
 
-    private List <String> dataList, dataList2, dataList3;
-    public MyAdapter(List<String> dataList,List<String> dataList2,List<String> dataList3){
+    private List <String> dataList, dataList2, dataList3,imagenes;
+    public MyAdapter(List<String> dataList,List<String> dataList2,List<String> dataList3,List<String> imagenes){
         this.dataList = dataList;
         this.dataList2 = dataList2;
         this.dataList3 = dataList3;
+        this.imagenes= imagenes;
     }
     @NonNull
     @Override
@@ -40,7 +44,18 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             holder.textView3.setBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.darkorange));
 
         }
-    }
+        String imagenUrl=imagenes.get(position); // detecto posicion de la lista
+
+        if (imagenUrl != null && !imagenUrl.isEmpty()) {
+            Glide.with(holder.itemView.getContext())
+                    .load(imagenUrl) // URL de la imagen desde Supabase
+                    .placeholder(R.drawable.usuariott) // Imagen por defecto mientras carga
+                    .error(R.drawable.usuariott) // Imagen en caso de error
+                    .into(holder.imageView);
+        } else {
+            holder.imageView.setImageResource(R.drawable.usuariott);
+        }
+        }
     @Override
     public int getItemCount(){
         return dataList.size();
@@ -48,13 +63,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     static class MyViewHolder extends RecyclerView.ViewHolder{
         TextView textView, textView2, textView3;
+        ImageView imageView;
         public MyViewHolder(@NonNull View itemView){
             super(itemView);
             textView = itemView.findViewById(R.id.textView_titulo);
             //AÑADIR DOS TEXTVIEW MAS ***************************************
             textView2 = itemView.findViewById(R.id.textView_direccion);
             textView3 = itemView.findViewById(R.id.textView_tipo);
-
+            imageView= itemView.findViewById(R.id.imageView);
         }
 
     }
